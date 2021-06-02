a5ff3121457be0d8e766bc5fd8f24f8ff34bf566
Arina Mykhova <arinabagira@mail.ru>
2021-06-02 19:33:46 +0300

    feat: сделать схему фильма

--- models/user.js
+++ models/user.js
@@ -2,26 +2,26 @@ const mongoose = require('mongoose');
 const isEmail = require('validator/lib/isEmail');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 2,
-        maxlength: 30,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        validate: {
-            validator: (v) => isEmail(v),
-            message: 'Неправильный формат почты',
-        },
-    },
-    password: {
-        type: String,
-        required: true,
-        select: false,
-    },
+	name: {
+		type: String,
+		minlength: 2,
+		maxlength: 30,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		validate: {
+			validator: (v) => isEmail(v),
+			message: 'Неправильный формат почты',
+		},
+	},
+	password: {
+		type: String,
+		required: true,
+		select: false,
+	},
 
 
 });
