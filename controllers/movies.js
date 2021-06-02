109f8db559a0ab25581e456052b5f7e2a1fac62a
Arina Mykhova <arinabagira@mail.ru>
2021-06-03 00:55:31 +0300

    feat: сделать централизованную обработку ошибок для movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,37 +1,50 @@
 const Movie = require('../models/movie');
+const NotFoundError = require('../errors/NotFoundError');
+const ForbiddenError = require('../errors/ForbiddenError');
+const InvalidRequestError = require('../errors/InvalidRequestError');
 
 module.exports.addMovie = (req, res, next) => {
     const { country, director, duration, year, description, image, trailer, nameRU, nameEN, thumbnail, movieId } = req.body;
     Movie.create({ country, director, duration, year, description, image, trailer, nameRU, nameEN, thumbnail, movieId, owner: req.user._id })
       .then((movie) => res.send(movie))
       .catch((err) => {
-        res.send(err.message)
-      });
+        if (err.name === 'CastError' || err.name === 'ValidationError') {
+          next(new InvalidRequestError('Переданы некорректные данные'));
+        } else {
+          next(err);
+        }
+      })
   };
 
-  module.exports.getMovies = (req, res) => {
+  module.exports.getMovies = (req, res, next) => {
     Movie.find({})
       .then((movie) => res.send(movie))
-      .catch((err) => {
-        res.send('Переданы некорректные данные')
-      });
+      .catch(next)
   };
 
   module.exports.deleteMovie = (req, res, next) => {
     Movie.findById(req.params.movieId)
       .then((movie) => {
         if (!movie) {
-          res.send('Фильм с указанным _id не найден');
+            throw new NotFoundError('Фильм с указанным _id не найден');
         } if (req.user._id !== movie.owner._id.toString()) {
-          res.send('Удалять можно только свои фильмы');
+            throw new ForbiddenError('Удалять можно только свои фильмы');
         }
         Movie.findByIdAndDelete(movie)
           .then(() => res.status(200).send({ message: 'Успешно' }))
           .catch((err) => {
-            res.send('Переданы некорректные данные')
+            if (err.name === 'CastError' || err.name === 'ValidationError') {
+              next(new InvalidRequestError('Переданы некорректные данные'));
+            } else {
+              next(err);
+            }
           });
       })
       .catch((err) => {
-        res.send('Переданы некорректные данные')
+        if (err.name === 'CastError' || err.name === 'ValidationError') {
+          next(new InvalidRequestError('Переданы некорректные данные'));
+        } else {
+          next(err);
+        }
       });
   };
