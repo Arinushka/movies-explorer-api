20f890343bb228214884f6cdd69a3801d40200ee
Arina Mykhova <arinabagira@mail.ru>
2021-06-06 19:44:38 +0300

    fix: поправить импорты

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const user = require('./users');
 const movie = require('./movies');
 const { createUser, login, signOut } = require('../controllers/users');
 const { registerValidation, authValidation } = require('../middlewares/bodyValidation');
-const NotFoundError = require('../../react-mesto-api-full/backend/errors/notFoundError');
 const { NOT_FOUND_ERROR } = require('../utils/constans');
+const NotFoundError = require('../errors/NotFoundError');
 
 router.post('/signup', registerValidation, createUser);
 router.post('/signin', authValidation, login);
